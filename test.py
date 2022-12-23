bca4a1f43c588b2a30f2485826c01013b7d67816
SotaChambers <snakamura@ridge-i.com>
2022-12-24 00:31:26 +0900

    仮作成

--- test.py
+++ test.py
@@ -127,3 +127,252 @@ def visualize_dataset(n_images, predict=None):
 
 split_ratio = 0.25
 train_size = int(np.round(train_dataset.__len__() * (1 - split_ratio), 0))
+valid_size = int(np.round(train_dataset.__len__() * split_ratio, 0))
+
+# データセットを [train, val] に分ける
+train_data, valid_data = random_split(train_dataset, [train_size, valid_size])
+
+train_loader = DataLoader(
+    dataset=train_data,
+    batch_size=10,
+    shuffle=True
+)
+val_loader = DataLoader(
+    dataset=valid_data,
+    batch_size=10
+)
+
+
+# UNet 
+class UNet(nn.Module):
+    def __init__(self, input_channels, output_channels):
+        super().__init__()
+        # 資料中の『FCN』に当たる部分
+        self.conv1 = conv_bn_relu(input_channels,64)
+        self.conv2 = conv_bn_relu(64, 128)
+        self.conv3 = conv_bn_relu(128, 256)
+        self.conv4 = conv_bn_relu(256, 512)
+        self.conv5 = conv_bn_relu(512, 1024)
+        self.down_pooling = nn.MaxPool2d(2)
+
+        # 資料中の『Up Sampling』に当たる部分
+        self.up_pool6 = up_pooling(1024, 512)
+        self.conv6 = conv_bn_relu(1024, 512)
+        self.up_pool7 = up_pooling(512, 256)
+        self.conv7 = conv_bn_relu(512, 256)
+        self.up_pool8 = up_pooling(256, 128)
+        self.conv8 = conv_bn_relu(256, 128)
+        self.up_pool9 = up_pooling(128, 64)
+        self.conv9 = conv_bn_relu(128, 64)
+        self.conv10 = nn.Conv2d(64, output_channels, 1)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight.data, a=0, mode='fan_out')
+                if m.bias is not None:
+                    m.bias.data.zero_()
+
+    def forward(self, x):
+        # 正規化
+        x = x/255.
+
+        # 資料中の『FCN』に当たる部分
+        x1 = self.conv1(x)
+        p1 = self.down_pooling(x1)
+        x2 = self.conv2(p1)
+        p2 = self.down_pooling(x2)
+        x3 = self.conv3(p2)
+        p3 = self.down_pooling(x3)
+        x4 = self.conv4(p3)
+        p4 = self.down_pooling(x4)
+        x5 = self.conv5(p4)
+
+        # 資料中の『Up Sampling』に当たる部分, torch.catによりSkip Connectionをしている
+        p6 = self.up_pool6(x5)
+        x6 = torch.cat([p6, x4], dim=1)
+        x6 = self.conv6(x6)
+
+        p7 = self.up_pool7(x6)
+        x7 = torch.cat([p7, x3], dim=1)
+        x7 = self.conv7(x7)
+
+        p8 = self.up_pool8(x7)
+        x8 = torch.cat([p8, x2], dim=1)
+        x8 = self.conv8(x8)
+
+        p9 = self.up_pool9(x8)
+        x9 = torch.cat([p9, x1], dim=1)
+        x9 = self.conv9(x9)
+
+        output = self.conv10(x9)
+        output = torch.sigmoid(output)
+
+        return output
+
+#畳み込みとバッチ正規化と活性化関数Reluをまとめている
+def conv_bn_relu(in_channels, out_channels, kernel_size=3, stride=1, padding=1):
+    return nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+    )
+
+def down_pooling():
+    return nn.MaxPool2d(2)
+
+def up_pooling(in_channels, out_channels, kernel_size=2, stride=2):
+    return nn.Sequential(
+        #転置畳み込み
+        nn.ConvTranspose2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride),
+        nn.BatchNorm2d(out_channels),
+        nn.ReLU(inplace=True)
+    )
+
+    
+class DiceBCELoss(nn.Module):
+    def __init__(self, weight=None, size_average=True):
+        super(DiceBCELoss, self).__init__()
+
+    def forward(self, inputs, targets, smooth=1):
+        
+        #comment out if your model contains a sigmoid or equivalent activation layer
+        inputs = F.sigmoid(inputs)       
+        
+        #flatten label and prediction tensors
+        inputs = inputs.view(-1)
+        targets = targets.view(-1)
+        
+        intersection = (inputs * targets).sum()                            
+        dice_loss = 1 - (2.*intersection + smooth)/(inputs.sum() + targets.sum() + smooth)  
+        BCE = F.binary_cross_entropy(inputs, targets, reduction='mean')
+        Dice_BCE = BCE + dice_loss
+        
+        return Dice_BCE
+
+
+class DiceLoss(nn.Module):
+    def __init__(self, weight=None, size_average=True):
+        super(DiceLoss, self).__init__()
+
+    def forward(self, inputs, targets, smooth=1):
+        
+        #comment out if your model contains a sigmoid or equivalent activation layer
+        #inputs = F.sigmoid(inputs)       
+        
+        #flatten label and prediction tensors
+        inputs = inputs.view(-1)
+        targets = targets.view(-1)
+        
+        intersection = (inputs * targets).sum()                            
+        dice = (2.*intersection + smooth)/(inputs.sum() + targets.sum() + smooth)  
+        
+        return 1 - dice
+    
+
+class IoU(nn.Module):
+    def __init__(self, weight=None, size_average=True):
+        super(IoU, self).__init__()
+
+    def forward(self, inputs, targets, smooth=1):
+        inputs = inputs.view(-1)
+        targets = targets.view(-1)
+        intersection = (inputs * targets).sum()
+        total = (inputs + targets).sum()
+        union = total - intersection 
+        
+        IoU = (intersection + smooth)/(union + smooth)
+                
+        return IoU
+
+
+def save_ckp(checkpoint, flag, checkpoint_path, best_model_path, ecpoch):
+    torch.save(
+        checkpoint,
+        f"{checkpoint_path}{epoch}.pt"
+    )
+    if flag:
+        torch.save(
+        checkpoint,
+        best_model_path
+    )
+
+#<---------------各インスタンス作成---------------------->
+model = UNet(3,1).cuda()
+optimizer = torch.optim.Adam(model.parameters(),lr = 1e-3)
+criterion = DiceLoss()
+accuracy_metric = IoU()
+num_epochs=20
+valid_loss_min = np.Inf
+
+checkpoint_path = 'model/chkpoint_'
+best_model_path = 'model/bestmodel.pt'
+
+total_train_loss = []
+total_train_score = []
+total_valid_loss = []
+total_valid_score = []
+
+losses_value = 0
+for epoch in range(num_epochs):
+    #<---------------トレーニング---------------------->
+    train_loss = []
+    train_score = []
+    valid_loss = []
+    valid_score = []
+    pbar = tqdm(train_loader, desc = 'description')
+    for x_train, y_train in pbar:
+        x_train = torch.autograd.Variable(x_train).cuda()
+        y_train = torch.autograd.Variable(y_train).cuda()
+        optimizer.zero_grad()
+        output = model(x_train)
+        ## 損失計算
+        loss = criterion(output, y_train)
+        losses_value = loss.item()
+        ## 精度評価
+        score = accuracy_metric(output,y_train)
+        loss.backward()
+        optimizer.step()
+        train_loss.append(losses_value)
+        train_score.append(score.item())
+        pbar.set_description(f"Epoch: {epoch+1}, loss: {losses_value}, IoU: {score}")
+    #<---------------評価---------------------->
+    with torch.no_grad():
+        for image,mask in val_loader:
+            image = torch.autograd.Variable(image).cuda()
+            mask = torch.autograd.Variable(mask).cuda()
+            output = model(image)
+            ## 損失計算
+            loss = criterion(output, mask)
+            losses_value = loss.item()
+            ## 精度評価
+            score = accuracy_metric(output,mask)
+            valid_loss.append(losses_value)
+            valid_score.append(score.item())
+
+    total_train_loss.append(np.mean(train_loss))
+    total_train_score.append(np.mean(train_score))
+    total_valid_loss.append(np.mean(valid_loss))
+    total_valid_score.append(np.mean(valid_score))
+    print(f"Train Loss: {total_train_loss[-1]}, Train IOU: {total_train_score[-1]}")
+    print(f"Valid Loss: {total_valid_loss[-1]}, Valid IOU: {total_valid_score[-1]}")
+
+    checkpoint = {
+        'epoch': epoch + 1,
+        'valid_loss_min': total_valid_loss[-1],
+        'state_dict': model.state_dict(),
+        'optimizer': optimizer.state_dict(),
+    }
+    
+    # checkpointの保存
+    save_ckp(checkpoint, False, checkpoint_path, best_model_path)
+    
+    # 評価データにおいて最高精度のモデルのcheckpointの保存
+    if total_valid_loss[-1] <= valid_loss_min:
+        print('Validation loss decreased ({:.6f} --> {:.6f}).  Saving model ...'.format(valid_loss_min,total_valid_loss[-1]))
+        save_ckp(checkpoint, True, checkpoint_path, best_model_path)
+        valid_loss_min = total_valid_loss[-1]
+
+    print("")
